bc36da888c8718878d7ce7c1f93f39f61aa925ad
Kanna Kamui <dragongm1981@gmail.com>
2025-07-03 17:44:36 +0700

    Change Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'akk1to.dev Profile',
-  description: 'Profile of akk1to.dev',
+  title: 'HunggVN Profile',
+  description: 'Profile of HunggVN',
 }
 
 export default function RootLayout({
